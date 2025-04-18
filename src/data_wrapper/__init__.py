246349c56bb6bb42b518eb67d44a675460fb1767
Victor Pasquini <victorcamposato@gmail.com>
2025-04-18 18:23:50 -0300

    ajuste init data wrapper para dataset alpaca

--- src/data_wrapper/__init__.py
+++ src/data_wrapper/__init__.py
@@ -2,4 +2,4 @@ from .data_wrapper import DataWrapper
 from .data_wrapper_glue import Glue
 from .data_wrapper_imdb import IMDB
 from .data_wrapper_swag import SWAG
-from .data_wrapper_alpaca import AlpacaDataset
+from .data_wrapper_alpaca import AlpacaDataset as Alpaca
